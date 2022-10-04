167938221c6825567855250e9d36f2f6da446370
VladisBeros <113285068+VladisBeros@users.noreply.github.com>
2022-10-04 22:03:33 +0300

    Add files via upload

---
+++ Lab4 (PD-21, Bero)/Lab4 (PD-21, Bero)/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Lab4__PD_21__Bero_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Даний масив розмiру N. Вивести його елементи в зворотному порядку.");
+            int[] M = { 0, 10, 20 };
+            Console.WriteLine("\nВывод в обычном порядке:");
+            for (int i = 0; i < M.Length; i++)
+            {
+                Console.WriteLine(M[i]);
+            }
+            Console.WriteLine("\nВывод в обратном порядке:");
+            for (int i = M.Length - 1; i >= 0; i--)
+            {
+                Console.WriteLine(M[i]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
